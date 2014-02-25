b9e5ebb5066d0449f91d04235f9333cbf7a00c97
Cody Rose <crose@bizstream.com>
2014-02-25 18:56:09 -0500

    HTML output now spits out options dynamically.

--- services/output.js
+++ services/output.js
@@ -1,25 +1,22 @@
 mfApp.factory('mapOutputService', function() {
 
-    var options =
-    [
-        {
-            "zoom": {
-                "value": 8,
-                "min": 1,
-                "max": 21,
-                "type": "number",
-                "label": "Zoom",
-                "default": 8,
-                "required": true
-            }
+    var options = {
+        zoom: {
+            value: 8,
+            min: 1,
+            max: 21,
+            type: "number",
+            label: "Zoom",
+            required: true,
+            default: 8
         },
-        {
-            "center": {
-                value: new google.maps.LatLng(-34.397, 150.644),
-                type: "object",
-                label: "Map Center",
-                required: true
-            }
+        center: {
+            value: new google.maps.LatLng(-34.397, 150.644),
+            type: "object",
+            typeText: "new google.maps.LatLng",
+            label: "Map Center",
+            required: true,
+            default: new google.maps.LatLng(-34.397, 150.644)
         },
         panControl: {
             value: true,
@@ -45,7 +42,7 @@ mfApp.factory('mapOutputService', function() {
             label: "Map Type Control",
             default: true
         }
-    ];
+    };
 
     var staticBeginHtml = '' +
         '<!DOCTYPE html>' +
@@ -68,26 +65,40 @@ mfApp.factory('mapOutputService', function() {
 
     var mapJS = function() {
 
-        var usedOptions = Enumerable.From(options).Where(function(o) { return o.Value.value != o.Value.default || o.Value.required == true }).ToArray();
-
-        angular.forEach(usedOptions, function(o) {
-            console.log(o.Key + ": " + o.Value.value);
-        });
+        var usedOptions = getUsedOptions();
 
         var js = '' +
             'var map;' +
             'function initialize() {' +
-            'var mapOptions = {' +
-            'zoom: ' + options.zoom.value + ',' +
-            'center: new google.maps.LatLng(-34.397, 150.644)' +
+            'var mapOptions = {';
+
+        angular.forEach(usedOptions, function(o, index) {
+
+            if (o.Value.type == "object")
+            {
+                js += o.Key + ": " + o.Value.typeText + o.Value.value.toString();
+            } else {
+                js += o.Key + ": " + o.Value.value;
+            }
+
+            if (index !== usedOptions.length) {
+                js += ",";
+            }
+
+        });
+
+        js +=
             '};' +
             'map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);' +
             '}' +
             'google.maps.event.addDomListener(window, "load", initialize);' +
             '';
 
-
         return js;
+    };
+
+    var getUsedOptions = function() {
+        Enumerable.From(options).Where(function(o) { return o.Value.value != o.Value.default || o.Value.required == true }).ToArray()
     };
 
     var genHtml = function() {
