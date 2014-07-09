d08d67d054b98e90d90cfbd7f5bd4ed3739fb954
Cody Rose <crose@bizstream.com>
2014-07-09 12:07:13 -0400

    Added routing, many other changes.

---
+++ controllers/codeview.js
@@ -0,0 +1,4 @@
+app.controller("CodeViewController", function($scope) {
+    $scope.phText = "This is where the code will go!!";
+});
+
---
+++ controllers/mapview.js
@@ -0,0 +1,13 @@
+app.controller("MapViewController", function($scope) {
+    var map = new L.Map('map');
+
+    // create the tile layer with correct attribution
+    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
+
+    // start the map in South-East England
+    map.setView(new L.LatLng(51.3, 0.7),9);
+    map.addLayer(osm);
+});
+
