01d6c3b592fc2b9b345c4203942feb72803a7a51
Cody Rose <codyjrose@gmail.com>
2014-08-13 18:23:05 -0400

    fixed up formatting

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 
 var app = angular.module('app', ['ngRoute'])
     .config(['$routeProvider',
-    function ($routeProvider) {
-        $routeProvider.
-            when('/options', { templateUrl: 'partials/options.html', controller: 'MapOptionsController' }).
-            when('/features', { templateUrl: 'partials/features.html',controller: 'MapFeaturesController' }).
-            when('/events', { templateUrl: 'partials/events.html', controller: 'MapEventsController' }).
-            when('/about', { templateUrl: 'partials/about.html' }).
-            otherwise({ redirectTo: '/options' });
-    }]);
+        function ($routeProvider) {
+            $routeProvider.
+                when('/options', { templateUrl: 'partials/options.html', controller: 'MapOptionsController' }).
+                when('/features', { templateUrl: 'partials/features.html', controller: 'MapFeaturesController' }).
+                when('/events', { templateUrl: 'partials/events.html', controller: 'MapEventsController' }).
+                when('/about', { templateUrl: 'partials/about.html' }).
+                otherwise({ redirectTo: '/options' });
+        }]);
