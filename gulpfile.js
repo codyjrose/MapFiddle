1bf4a70a98c924f7539e9395742028865f96ede6
Cody Rose <codyjrose@gmail.com>
2014-08-17 13:23:01 -0400

    reorganized files, updated gulp file to handle or better handle js, css and html

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,77 @@
 // Include gulp
 var gulp = require('gulp');
 
-// Include Our Plugins
+// Include plugins
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var rimraf = require('gulp-rimraf');
+var streamqueue = require('streamqueue');
+var minifyCSS = require('gulp-minify-css');
+var htmlreplace = require('gulp-html-replace');
 
-// Concatenate & Minify JS
+
+// Concatenate & minify JS
 gulp.task('scripts', function () {
-    gulp.src(['js/**/*.js', '!js/**/*.min.js'])
-        .pipe(concat('app.js'))
+    return gulp.src(['src/js/**/*.js', '!src/js/vendor/*.js'])
+        .pipe(concat('scripts.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist/js'));
+});
 
-    gulp.src('js/vendor/*.js')
-        .pipe(concat('vendor.js'))
-        .pipe(gulp.dest('dist'));
+// Concatenate & minify CSS and copy fonts
+gulp.task('css', function () {
+    // Copy fonts to dist/css folder
+    gulp.src(['src/css/*', '!src/css/*.css'])
+        .pipe(gulp.dest('dist/css'));
 
-    gulp.src('dist/*.js')
-        .pipe(concat('all.min.js'))
-        .pipe(gulp.dest('dist'));
+    var stream = streamqueue({ objectMode: true });
 
-    gulp.src(['dist/*.js', '!dist/*.min.js'], { read: false })
-        .pipe(rimraf({ force: true }));
+    stream.queue(
+        gulp.src(['src/css/*.css', '!src/css/*.min.css'])
+            .pipe(minifyCSS())
+    );
 
+    stream.queue(
+        gulp.src('src/css/*.min.css')
+    );
+
+    return stream.done()
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('dist/css'));
+});
+
+// Copy HTML files to dist folder
+gulp.task('html', function () {
+    // Copy fonts to dist/css folder
+    gulp.src(['src/**/*.html'])
+        .pipe(htmlreplace({
+            'js': 'js/scripts.js',
+            'cdn': [
+                '//cdnjs.cloudflare.com/ajax/libs/prettify/r298/run_prettify.min.js',
+                '//cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.min.js',
+                '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular.min.js',
+                '//ajax.googleapis.com/ajax/libs/angularjs/1.2.20/angular-route.min.js',
+                '//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js',
+                '//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.js'
+            ],
+            'css': 'css/styles.css'
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+// Copy HTML files to dist folder
+gulp.task('assets', function () {
+    // Copy fonts to dist/css folder
+    gulp.src(['src/assets/**/*.*'])
+        .pipe(gulp.dest('dist/assets'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function () {
-    gulp.watch('js/*.js', 'scripts');
+    gulp.watch('src/js/*.js', 'scripts');
 });
 
 // Default Task
-gulp.task('default', ['scripts', 'watch']);
+gulp.task('default', ['scripts', 'css', 'watch']);
+gulp.task('build', ['scripts', 'css', 'html', 'assets']);
