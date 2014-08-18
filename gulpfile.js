b323c24a154d64eaf82f3edc23c2d1a37f04d964
Cody Rose <crose@bizstream.com>
2014-08-18 11:33:09 -0400

    Added google analytics, removed blank lines from index.html

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,8 @@ gulp.task('html', function () {
                 '//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js',
                 '//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.js'
             ],
-            'css': 'css/styles.css'
+            'css': 'css/styles.css',
+            'ga': "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-38807124-2', 'auto');ga('send', 'pageview');</script>"
         }))
         .pipe(gulp.dest('dist/'));
 });
